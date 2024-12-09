5dbd925ad6ed7dcba17786833b1d05658e59ec48
eucarizan <eucarizan@gmail.com>
2024-12-09 14:14:54 +0800

    stage 1 done

---
+++ stage-01.js
@@ -0,0 +1,17 @@
+const input = require('sync-input');
+
+// We have imported the 'sync-input' package for you.
+// This package allows you to get user input.
+// Like so:
+// let name = input("Type your name: ");
+// let age = Number(input("Type your age: "));
+// You will need it in later stages.
+
+console.log("Prices:")
+console.log("Bubblegum: $2")
+console.log("Toffee: $0.2")
+console.log("Ice cream: $5")
+console.log("Milk chocolate: $4")
+console.log("Doughnut: $2.5")
+console.log("Pancake: $3.2")
+
